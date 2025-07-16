7b8c49a1e9c6349bb64bd87f4bcce29f19aa7e84
1vasi <yungvasi1@gmail.com>
2025-07-16 12:44:55 +0300

    functionality updated

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 from constants import *
 pygame.init()
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    clock = pygame.time.Clock()
+    dt = 0 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    
+    Player.containers = (updatable, drawable)
+    
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while running: 
         for event in pygame.event.get():
@@ -17,8 +24,13 @@ def main():
                 return
             
         screen.fill("black")
-    
 
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip() 
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
    main()
